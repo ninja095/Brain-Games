8cb7e4ef7b9650a8b5253ad315c16c6c91417f55
ninja095 <ninja095@gmail.com>
2022-04-07 11:57:10 +0200

    sttep 3

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const userName = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+}
+export default userName();
