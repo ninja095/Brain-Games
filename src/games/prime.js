b56fac78c292b69595ff37dbe52275f8a4a2c1ca
ninja095 <ninja095@gmail.com>
2022-04-18 10:26:08 +0200

    fix prime.js logic

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,7 +5,7 @@ const rule = '"yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
   if (num <= 1) return false;
-  for (let i = 2; i < num / 2; i += 1) {
+  for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) return false;
   }
   return true;
@@ -14,7 +14,7 @@ const isPrime = (num) => {
 const questionAndAnswer = () => {
   const question = getRandomNumber(1, 10);
   const answer = isPrime(question) ? 'yes' : 'no';
-  return [question, String(answer)];
+  return [question, answer];
 };
 
 const startPrime = () => generalLogic(rule, questionAndAnswer);
