df21624905d35ff046fb339eb820c3194e24736e
ninja095 <ninja095@gmail.com>
2022-04-20 14:15:33 +0200

    added brain-games

--- Makefile
+++ Makefile
@@ -8,6 +8,9 @@ publish:
 lint:
 	npx eslint .
 
+brain-games:
+	node bin/brain-games.js
+
 brain-even:
 	node bin/brain-even.js
 
---
+++ bin/brain-games.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
+const userName = readlineSync.question('May I have your name? ');
+
+console.log(`Hello, ${userName}!`);
